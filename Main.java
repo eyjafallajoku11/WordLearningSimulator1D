1c944a3ea23259127389e9dfbd47e27d61e636c9
eyjafallajoku11 <savelov.art@gmail.com>
2023-03-28 14:44:09 +0200

    word selection

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        // Create an instance of the WordManager class with the provided command line arguments
         WordManager manager = new WordManager(args);
+        // Print the current state of the WordManager object
         System.out.println(manager);
-        for (short i = 0; i<100; i++) {
-            System.out.println(manager.getWord());
+        // Keep playing the game until all the words have been learned
+        while (!manager.allLearned()){
+            // Get a random word and prompt the user to input the correct spelling
+            manager.getWord();
+            // Print the current state of the WordManager object
+//            System.out.println(manager);
         }
     }
 }
--- WordManager.java
+++ WordManager.java
@@ -1,31 +1,93 @@
+// Import necessary libraries
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class WordManager {
-    private List<List<String>> words_dataset = new ArrayList<>();
+    private final List<List<String>> words_dataset = new ArrayList<>();
     private final int wordCount;
+
+    // Create a Scanner object to read user input from the console
+    Scanner scanner = new Scanner(System.in);
+
+    // Constructor to initialize the WordManager object with an array of words
     public WordManager(String[] words) {
         wordCount = words.length;
         for (short i = 0; i < 5; i++) {
-            if (i == 2){
+            // Add all the words to the second difficulty level
+            if (i == 1){
                 words_dataset.add(new ArrayList<>(List.of(words)));
             }
+            // Add empty lists for the other difficulty levels
             else words_dataset.add(new ArrayList<>());
         }
     }
-    public String getWord(){
-        short n = 0 ;
+
+    // Method to select and display a random word from the WordManager's dataset
+    // prompt the user to enter a guess
+    // move the word to the appropriate difficulty level based on the user's response
+    public void getWord(){
+        // Initializing for a variable for selecting the difficulty level of the word
+        short n = 0;
+
+        // Track the total number of words seen so far
         int count = 0;
-        for (short i = 0; i<4;i++){
-            n=i;
+
+        // Iterate through the 5 difficulty levels
+        for (short i = 0; i<5; i++){
+            // Get the number of words in the current difficulty level
             int t = words_dataset.get(i).size();
-            count+=t;
-            if (Math.random()<0.5 & t!=0 | count == wordCount){
+
+            // Increment the total count of words seen so far
+            count += t;
+
+            // Check if a word should be selected from the current difficulty level
+            if ((Math.random() < 0.5 & t != 0) | count == wordCount){
+                // If so, set the current difficulty level index and exit the loop
+                n=i;
                 break;
             }
         }
+
+        // Select a random word from the chosen difficulty level
         int m = (int) (Math.random() * words_dataset.get(n).size());
-        return words_dataset.get(n).get(m);
+        String word = words_dataset.get(n).get(m);
+
+        // Output the selected word to the console
+        System.out.println(word);
+
+        // Prompt the user to enter their guess for the word
+        String inputString = scanner.nextLine();
+
+        // Check if the user's guess matches the selected word and output the result to the console
+        System.out.println("You entered: " + inputString + " it's " + inputString.equals(word));
+
+        // Move the selected word to the appropriate difficulty level based on whether the user's guess was correct or not
+        moveWord(n, m, inputString.equals(word));
+    }
+
+    // Method to move the selected word to the appropriate difficulty level based on whether the user's guess was correct or not
+    private void moveWord(int n, int m, boolean isCorrect){
+        String temp;
+        if (isCorrect){
+            // Move the word up one difficulty level if the user's guess was correct and the word is not already in the highest difficulty level
+            if (n != 4){
+                temp = words_dataset.get(n).remove(m);
+                words_dataset.get(n+1).add(temp);
+            }
+        }
+        else {
+            // Move the word down one difficulty level if the user's guess was incorrect and the word is not already in the lowest difficulty level
+            if (n != 0){
+                temp = words_dataset.get(n).remove(m);
+                words_dataset.get(n-1).add(temp);
+            }
+        }
+    }
+
+    // Method to check if all the words have been learned (i.e. are in the highest difficulty level)
+    public Boolean allLearned(){
+        return wordCount == words_dataset.get(4).size();
     }
     @Override
     public String toString() {
